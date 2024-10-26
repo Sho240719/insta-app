4e6acdbd655c8d4a42947fe221e879f7279c25f9
Sho <sho84.kitzxo@gmail.com>
2024-10-27 06:43:29 +0900

    editの作成

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,15 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_profile
 
   def show
+  end
+
+  def edit
+  end
+
+  private
+  def set_profile
+    @profile = profile || build_profile
   end
 end
