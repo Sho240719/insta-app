848f2aa45e2982a8f4994748e7fcb31017df2ddf
Sho <sho84.kitzxo@gmail.com>
2024-10-29 15:33:20 +0900

    jQueryの追加、webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import $ from 'jquery'
+
+document.addEventListener('DOMContentLoaded', () => {
+  window.alert('Hello')
+})
