273ebddca072aa1e28edaee469c023e085f0093e
Sho <sho84.kitzxo@gmail.com>
2024-11-07 21:36:42 +0900

    createアクション

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,19 @@ class PostsController < ApplicationController
   def new
     @post = current_user.posts.build
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path, notice: '投稿しました'
+    else
+      flash.now[:error] = '投稿できませんでした'
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content, images: [])
+  end
 end
