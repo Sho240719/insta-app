93665856d10f7dec087f037453f52a323e4b8084
Sho <sho84.kitzxo@gmail.com>
2024-11-10 00:46:36 +0900

    Likeモデルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,7 @@ class User < ApplicationRecord
 
   has_one :profile, dependent: :destroy
   has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   def avatar_image
     if profile&.avatar&.attached?
