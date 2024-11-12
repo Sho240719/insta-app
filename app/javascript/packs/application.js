74719692d546cbd4575418faae5e71af567ba9eb
Sho <sho84.kitzxo@gmail.com>
2024-11-12 17:08:16 +0900

    ajaxでいいね状態を作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,9 +18,12 @@ require("channels")
 
 import $ from 'jquery'
 import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
 // いいね表示を切り替えるfunctionを定義
-const hundleHeartDisplay = (hasliked) => {
+const hundleHeartDisplay = (hasLiked) => {
   if (hasLiked) {
     $('.active-heart').removeClass('hidden')
   } else {
@@ -62,7 +65,7 @@ document.addEventListener('turbolinks:load', () => {
   });
 
 
-  // JSでいいね機能を実装
+  // いいねの状態を取得
   const dataset = $('.post-show').data()
   const postId = dataset.postId
 
@@ -71,4 +74,30 @@ document.addEventListener('turbolinks:load', () => {
       const hasLiked = response.data.hasLiked
       hundleHeartDisplay(hasLiked)
     })
+
+  // いいねの表示を切り替え
+  // いいねをする
+  $('.inactive-heart').on('click', () => {
+    axios.post(`/posts/${postId}/like`)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((e) => {
+        window.alert('Error')
+        console.log(e)
+      })
+  })
+
+  // いいねをはずす
+  $('.active-heart').on('click', () => {
+    axios.delete(`/posts/${postId}/like`)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((e) => {
+        window.alert('Error')
+        console.log(e)
+      })
+  })
+
 })
