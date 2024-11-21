1b9486c1c6ba085aea07796c95448d51b8ead1e3
Sho <sho84.kitzxo@gmail.com>
2024-11-21 22:47:57 +0900

    コールバックでメールを送付

--- app/mailers/mention_mailer.rb
+++ app/mailers/mention_mailer.rb
@@ -2,6 +2,9 @@ class MentionMailer < ApplicationMailer
   def send_mention_notification(recipient_user, sender_user)
     @recipient_user = recipient_user
     @sender_user = sender_user
-    mail to: recipient_user.email, subject: '【お知らせ】メッセージがあります'
+
+    subject = "#{@sender_user.account_name}さんがあなたにメッセージを送信しました"
+
+    mail to: recipient_user.email, subject: subject
   end
 end
