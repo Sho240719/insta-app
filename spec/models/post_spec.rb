ce9afeb7fea73521bd59ed77959561d79841a3dc
Sho <sho84.kitzxo@gmail.com>
2024-12-08 07:36:44 +0900

    post factoryを登録

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,11 +4,7 @@ RSpec.describe Post, type: :model do
   let!(:user) { create(:user) }
 
   context '内容が入力されている場合' do
-    let!(:post) do
-      user.posts.build({
-        content: Faker::Lorem.characters(number: 20)
-      })
-    end
+    let!(:post) { build(:post, user: user) }
 
     it 'ポストを保存できる' do
       expect(post).to be_valid
@@ -16,10 +12,10 @@ RSpec.describe Post, type: :model do
   end
 
   context '内容が一文字の場合' do
-    let!(:post) do
-      user.posts.create({
-        content: Faker::Lorem.characters(number: 1)
-      })
+    let!(:post) { build(:post, content: Faker::Lorem.characters(number: 1), user: user) }
+
+    before do
+      post.save
     end
 
     it 'ポストを保存できない' do
