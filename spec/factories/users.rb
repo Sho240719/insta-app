6532024f002ef1eaf3faf5547b8ba24d1826279a
Sho <sho84.kitzxo@gmail.com>
2024-12-12 16:45:09 +0900

    system specでログイン時にプロフィールが表示されるか確認

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
     account_name { Faker::Name.name }
     email { Faker::Internet.email }
     password { 'password' }
+
+    trait :with_profile do
+      after :build do |user|
+        build(:profile, user: user)
+      end
+    end
   end
 end
