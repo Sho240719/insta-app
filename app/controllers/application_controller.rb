cb84bbffb1e158002bca15e9fc15bc446090c95b
Sho <sho84.kitzxo@gmail.com>
2024-12-16 15:48:12 +0900

    ユーザー新規登録時にユーザーネームが登録できないバグを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # devise機能、新規登録時にアカウントネームも含めるように
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    # Sign up時にaccount_nameを許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:account_name])
+  end
 end
